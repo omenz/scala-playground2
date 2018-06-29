8bcfb516fea0be5c3ba5c4d47d095d1a2f6628ce
Alexander Krasovsky <ak@weissenrieder.com>
2018-06-29 12:53:44 +0300

    AbstractDataTypes.scala

---
+++ src/lectures/part2oop/AbstractDataTypes.scala
@@ -0,0 +1,37 @@
+package lectures.part2oop
+
+/**
+  * Created by Alexander Krasovsky on 29.06.2018.
+  */
+object AbstractDataTypes extends App {
+
+  //abstract, can have both abstract and non abstract methods and attributes
+  abstract class Animal {
+    val creatureType: String
+    def eat: Unit
+  }
+
+
+  class Dog extends Animal {
+    override val creatureType: String = "Canine"
+    override def eat: Unit = println("crunch crunch")
+  }
+
+  // traits, do not have constructions parameters
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  trait ColdBlooded
+
+  //can have multiple traits and can extend one Class
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val creatureType: String = "Reptile"
+    override def eat: Unit = println("nomnomnonm")
+    override def eat(animal: Animal): Unit = println(s"I'ma croc and I'm eating ${animal.creatureType}")
+  }
+
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
+}
---
+++ src/lectures/part2oop/Inheritance.scala
@@ -0,0 +1,32 @@
+package lectures.part2oop
+
+/**
+  * Created by Alexander Krasovsky on 27.06.2018.
+  */
+object Inheritance extends App {
+
+  //constructors
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name, 0)
+  }
+
+  class Adult(name: String, age: Int, idCard: String) extends Person(name, age)
+
+  class Animal(val creatureType: String) {
+    protected def eat(): Unit = println("nomnom")
+  }
+
+  class Dog(override val creatureType: String) extends Animal(creatureType) {
+    override def eat(): Unit = println("crunch crunch")
+  }
+
+  val dog = new Dog("K9")
+  dog.eat
+
+  //type substitution
+  val unknownAnimal = new Dog("K9")
+  unknownAnimal.eat
+
+  //preventing overriding: use "final" on class members or class.
+  //use keywords "seal" - allows extending classes only in the same file
+}
