6dc17464f15e5f87fde21386d8215135cbc24945
Alexander Krasovsky <ak@weissenrieder.com>
2018-06-30 14:33:53 +0300

    MyList.scala

--- src/excercises/MyList.scala
+++ src/excercises/MyList.scala
@@ -3,29 +3,29 @@ package excercises
 /**
   * Created by Alexander Krasovsky on 29.06.2018.
   */
-abstract class MyList() {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A]() {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   //polymorphic call
   override def toString: String = s"[$printElements]"
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException
-  override def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  override def head: Nothing= throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(element: Int): MyList = new Cons(element, Empty)
+  override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   override def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
-  override def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
+  override def tail: MyList[A] = t
   override def isEmpty: Boolean = false
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   override def printElements: String =
     if (t.isEmpty) "" + h
@@ -34,9 +34,11 @@ class Cons(h: Int, t: MyList) extends MyList {
 
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val list: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons("pew", new Cons("pew2", new Cons("pew3", Empty)))
   println(list.tail.head)
   println(list.add(4).head)
   println(list.isEmpty)
   println(list)
+  println(listOfStrings)
 }
