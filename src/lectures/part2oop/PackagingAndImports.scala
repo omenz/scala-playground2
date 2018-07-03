c3dfbf8d758c841e9b5f9171e2d3b600744e310a
Alexander Krasovsky <ak@weissenrieder.com>
2018-07-03 20:01:08 +0300

    PackagingAndImports.scala

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,21 @@
+package lectures.part2oop
+
+import excercises.{Cons, Empty => RenamedEmpty}//it is possible to rename imports
+
+/**
+  * Created by Alexander Krasovsky on 03.07.2018.
+  */
+object PackagingAndImports extends App {
+
+  //taken from the package object
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+  val myList = new Cons[Int](1, RenamedEmpty)
+
+
+  //default imports
+  //java.lang - String, Object, Exception
+  //scala - Int, Nothing, Function
+  //scala.Predef - println, ???
+}
