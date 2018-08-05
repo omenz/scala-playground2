6321d81ffd7ea5ad0b74876836b0323b3ca101c5
Aleksandrs Krasovskis <akrasovskis@evolutiongaming.com>
2018-08-05 09:37:49 +0300

    HandlingFailure.scala

---
+++ src/lectures/part3fp/HandlingFailure.scala
@@ -0,0 +1,92 @@
+package lectures.part3fp
+
+import scala.util.{Failure, Random, Success, Try}
+
+object HandlingFailure extends App {
+
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("Super failure"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("pew pew")
+
+  //Try objects via the apply method
+  val potentialFailure = Try(unsafeMethod())
+  println(potentialFailure)
+
+  //syntax sugar
+  val anotherPotentialFailure = Try {
+    // code that might throw
+  }
+
+
+  //utilities
+  println(potentialFailure.isSuccess)
+
+  def backupMethod(): String = "A valid result"
+  val fallbackTry = Try(unsafeMethod()) orElse Try(backupMethod())
+  println(fallbackTry)
+
+
+  //if you design the API
+  def betterUnsafeMethod(): Try[String] = Failure(new RuntimeException)
+  def betterBackupMethod(): Try[String] = Success("Some string")
+  val betterFallback = betterUnsafeMethod() orElse betterBackupMethod()
+  println(betterFallback)
+
+  //map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 6)))
+  println(aSuccess.filter(_ > 10))
+
+  /*
+    Excercise
+  */
+
+  val host = "localhost"
+  val port = "8080"
+  def renderHTML(page: String): Unit = println(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("Connection interrupted")
+    }
+
+    def getSafe(url: String): Try[String] = Try(get(url))
+  }
+
+  object HttpService {
+    private val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException(s"Port $port is already in use")
+    }
+
+    def getSafeConnection(host: String, port: String): Try[Connection] = Try(getConnection(host, port))
+  }
+
+  //chain calls with unsafe api
+  Try(HttpService.getConnection(host, port))
+    .flatMap(connection => Try(connection.get("someurl")))
+    .foreach(page => renderHTML(page))
+
+  val possibleConnection = HttpService.getSafeConnection(host, port)
+  val possibleHTML = possibleConnection.flatMap(connection => connection.getSafe("/home"))
+  possibleHTML.foreach(renderHTML)
+
+  //chain calls
+  HttpService.getSafeConnection(host, port)
+    .flatMap(connection => connection.getSafe("/home"))
+    .foreach(renderHTML)
+
+  //for-comprehension
+  for {
+    connection <- HttpService.getSafeConnection(host, port)
+    html <- connection.getSafe("/home")
+  } renderHTML(html)
+}
