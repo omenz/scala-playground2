944fdb531efbcf510d4e1ff1373ccca35e8a8699
alex.k <alex.k>
2018-08-26 19:53:12 +0300

    ThreadCommunication.scala

---
+++ src/advanced/lectures/part3concurrency/ThreadCommunication.scala
@@ -0,0 +1,80 @@
+package advanced.lectures.part3concurrency
+
+object ThreadCommunication extends App {
+
+
+  /*
+    the producer-consumer problem
+
+    producer -> [ ? ] -> consumer
+   */
+
+  class SimpleContainer {
+    private var value: Int = 0
+
+    def isEmpty: Boolean = value == 0
+    def set(newValue: Int) = value = newValue
+    def get = {
+      val result = value
+      value = 0
+      result
+    }
+  }
+
+  def naiveProdCons(): Unit = {
+    val container = new SimpleContainer
+
+    val consumer = new Thread(() => {
+      println("[consumer] waiting...")
+      while (container.isEmpty) {
+        println("[consumer] actively waiting...")
+      }
+
+      println(s"[consumer] I have consumed ${container.get}")
+    })
+
+    val producer = new Thread(() => {
+      println("[producer] computing...")
+      Thread.sleep(500)
+      val value = 42
+      println(s"[producer] I have produced, after long work, the value $value")
+      container.set(value)
+    })
+
+    consumer.start()
+    producer.start()
+  }
+
+//  naiveProdCons()
+
+  // wait and notify
+  def smartProdCons(): Unit = {
+    val container = new SimpleContainer
+
+    val consumer = new Thread(() => {
+      println("[consumer] Waiting...")
+      container.synchronized {
+        container.wait()
+      }
+      // container must have some value
+      println("[consumer] I have consumed " + container.get)
+    })
+
+    val producer = new Thread(() => {
+      println("[producer] Hard at work...")
+      Thread.sleep(2000)
+      val value = 42
+
+      container.synchronized {
+        println(s"[producer] I'm producing $value")
+        container.set(value)
+        container.notify()
+      }
+    })
+
+    consumer.start()
+    producer.start()
+  }
+
+  smartProdCons()
+}
