d22967ff3d92ce6d33be1264878f4addc7939b05
alex.k <alex.k>
2018-08-28 21:08:02 +0300

    FuturePromises.scala

---
+++ src/advanced/lectures/part3concurrency/FuturesPromises.scala
@@ -0,0 +1,27 @@
+package advanced.lectures.part3concurrency
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object FuturesPromises extends App {
+
+  def calculateMeaningOfLife: Int = {
+    Thread.sleep(2000)
+    42
+  }
+
+  val aFuture = Future {
+    calculateMeaningOfLife // calculates the meaning of life on ANOTHER thread
+  } // (global) which is passed by the compiler
+
+  println(aFuture.value) // Option[Try[Int]]
+
+  println("Waiting on the future")
+  aFuture.onComplete {
+    case Success(meaningOfLife) => println(s"the meaning of life is $meaningOfLife")
+    case Failure(e) => println(s"I have failed with $e")
+  } // SOME thread
+  Thread.sleep(3000)
+}
