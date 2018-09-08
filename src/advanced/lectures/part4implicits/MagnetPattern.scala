8fa3eb29deada0f4b5e0ca2169b5bd827b7b4784
alex.k <alex.k>
2018-09-08 10:44:39 +0300

    JSONSerialization.scala

---
+++ src/advanced/lectures/part4implicits/MagnetPattern.scala
@@ -0,0 +1,112 @@
+package advanced.lectures.part4implicits
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object MagnetPattern extends App {
+
+
+  // method overloading
+
+  class P2PRequest
+  class P2PResponse
+  class Serializer[T]
+
+
+  trait Actor {
+    def receive(statusCode: Int): Int
+    def receive(request: P2PRequest): Int
+    def receive(response: P2PResponse): Int
+    def receive[T : Serializer](message: T): Int
+    def receive[T : Serializer](message: T, statusCode: Int): Int
+    def receive(future: Future[P2PRequest]): Int
+//    def receive(future: Future[P2PResponse]): Int //type erasure, not possible to compile
+    // lots of overloads
+  }
+
+  /*
+    Problems with this approach:
+    1 - type erasure
+    2 - lifting doesn't work for all overloads
+
+    val receiveFV = receive _ // ?
+
+    3 - code duplication
+    4 - type inferrence and default args
+    actor.receive(?!)
+   */
+
+  trait MessageMagnet[Result] {
+    def apply(): Result
+  }
+
+  def receive[R](magnet: MessageMagnet[R]): R = magnet()
+
+  implicit class FromP2PRequest(request: P2PRequest) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      println("handling p2p request")
+      42
+    }
+  }
+
+  implicit class FromP2PResponse(request: P2PResponse) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      println("handling p2p response")
+      24
+    }
+  }
+
+  receive(new P2PRequest)
+  receive(new P2PResponse)
+
+
+  // 1 - no more type erasure problems!
+  implicit class FromResponseFuture(future: Future[P2PResponse]) extends MessageMagnet[Int] {
+    override def apply(): Int = 2
+  }
+
+  implicit class FromRequestFuture(future: Future[P2PRequest]) extends MessageMagnet[Int] {
+    override def apply(): Int = 3
+  }
+
+  println(receive(Future(new P2PRequest)))
+  println(receive(Future(new P2PResponse)))
+
+  // 2 - lifting works
+  trait MathLib {
+    def add1(x: Int) = x + 1
+    def add1(s: String) = s.toInt + 1
+    // add1 overloads
+  }
+
+  // "magnetize"
+  trait AddMagnet {
+    def apply(): Int
+  }
+
+  def add1(magnet: AddMagnet): Int = magnet()
+
+  implicit class AddInt(x: Int) extends AddMagnet {
+    override def apply(): Int = x + 1
+  }
+
+  implicit class AddString(s: String) extends AddMagnet {
+    override def apply(): Int = s.toInt + 1
+  }
+
+  val addFV = add1 _ // return type is always Int
+  println(addFV(1))
+  println(addFV("3"))
+
+  val receiveFV = receive _
+// receiveFV(new P2PResponse) // compiler can't compile becase return type is unknown
+
+  /*
+    Drawbacks
+    1 - verbose
+    2 - harder to read
+    3 - you can't name or place default arguments
+    4 - call by name doesn't work correctly
+    (exercise: prove it!) (hint: side effects)
+   */
+}
