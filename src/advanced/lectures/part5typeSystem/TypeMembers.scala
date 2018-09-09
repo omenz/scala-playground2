342b37804dc535cecc99e7ff75a4190091a2ea27
alex.k <alex.k>
2018-09-09 19:27:05 +0300

    TypeMembers.scala

---
+++ src/advanced/lectures/part5typeSystem/TypeMembers.scala
@@ -0,0 +1,72 @@
+package advanced.lectures.part5typeSystem
+
+object TypeMembers extends App {
+
+  class Animal
+  class Dog extends Animal
+  class Cat extends Animal
+
+  class AnimalCollection {
+    type AnimalType // abstract type member
+    type BoundedAnimal <: Animal // upper bounded with animal
+    type SuperBoundedAnimal >: Dog <: Animal
+    type AnimalC = Cat // another name for existing typ
+  }
+
+  val ac = new AnimalCollection
+//  val dog: ac.AnimalType = ???
+
+//  val cat: ac.BoundedAnimal = new Cat // won't compile
+
+  val pup: ac.SuperBoundedAnimal = new Dog
+  val cat: ac.AnimalC = new Cat
+
+  type CatAlias = Cat
+  val anotherCat: CatAlias = new Cat
+
+  // alternative to generics
+  trait MyList {
+    type T
+    def add(element: T): MyList
+  }
+
+  class NonEmptyList(value: Int) extends MyList {
+    override type T = Int
+    def add(element: Int): MyList = ???
+  }
+
+  // .type
+  type CatsType = cat.type
+  val newCat: CatsType = cat
+//  new CatsType
+
+  /*
+    Exercise - enforce a type to be applicable to SOME TYPES only
+   */
+  trait MList {
+    type A
+    def head: A
+    def tail: MList
+  }
+  trait ApplicableToNumbers {     // solution, add ApplicableToNumbers to allow only numbers
+    type A <: Number
+  }
+
+
+  // NOT OK
+  class CustomList(hd: String, tl: CustomList) extends MList with ApplicableToNumbers {
+    type A = String
+    def head = hd
+    def tail = tl
+  }
+
+  //OK
+  class IntList(hd: Int, tl: IntList) extends MList {
+    type A = Int
+    def head = hd
+    def tail = tl
+  }
+
+  // Number
+  // type members and type member constraints (bounds)
+}
